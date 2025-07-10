a2cd6a7fb4d7b3babd91928e86723e977e0a8ef0
Dmytro Marych <dimamarich07@gmail.com>
2025-07-10 20:34:39 +0300

    added add function, it needs to be done(to do: connect to json)

--- task-cli.py
+++ task-cli.py
@@ -3,7 +3,14 @@ import sys
 
 def operations(args):
     command = args[0]
+    if command == 'add':
+        add_op(args[1])
 
+
+def add_op(description):
+#     adding to json
+    task_id = 1 #temp
+    print(f'Task added succesfully (ID: {task_id})')
 
 
 if __name__ == '__main__':
